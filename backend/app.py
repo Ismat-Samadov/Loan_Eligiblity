4d131df3bda73721170174275ff6fd0382cc9a96
Ismat <ismetsemedov@gmail.com>
2024-09-27 08:49:27 +0400

    check

--- backend/app.py
+++ backend/app.py
@@ -98,5 +98,6 @@ def predict():
         "input_data": mapped_data
     })
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host='0.0.0.0', port=port)
